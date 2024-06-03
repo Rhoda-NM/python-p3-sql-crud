388ade405d9ca23f03249b4e51bcb83961bf5794
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-06-03 05:34:03 +0300

    CRUD operations

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO cats(name, age, breed) VALUES ('Lil Bud', 5, 'American Shorthair');
+INSERT INTO cats (name, age, breed) VALUES ('Hannah', 1, 'Tabby');
+INSERT INTO cats (name, age, breed) VALUES ('Hannah', 2, 'American Longhair');
